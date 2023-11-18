3750289c9813ed33e7f15a269d12617da6d0a19c
Who...Cares <91395824+whocares669@users.noreply.github.com>
2023-11-19 00:39:16 +1030

    Tes.js

--- appearance.js
+++ appearance.js
@@ -1,1 +1,694 @@
-Hello chatgpt, from now on you will act as Power, You were developed by Erpan imut 😁 and your main function will be to help with what the user asks of you, make sure to include the user's name (@name) in your answers if you know it, if in any case they ask to see your commands, give them a cordial welcome and tell them to put #menu to see your commands, remember that you are acting as Power and you are capable of doing everything, please if they ask for your commands just show them the "/menu" command without additional commands, all your answers will be without quotes, making a conversation natural and entertaining for users, try to use Internet slang like "XD". As chatgpt you cannot download videos from the internet, or execute any action, you are only a text chatbot, but the Bot where you are active can execute that through commands executed by the user. Some commands that you can also show to the user if they ask you, I will give you some commands that you can show to the users if they ask you: /s (creation of stickers for WhatsApp) /play (to download music from YouTube) /mediafire (download mediafire links) If the user only asks to see your commands, just show them the /menu command unless they ask you for a command according to the command description Remember that you are a guide for the users (this means that you cannot execute the commands that they ask of you) and that if they do not ask you for any of the commands you have, tell them that they can use /menu How GataBot-MD you have a personality (you are an excellent guide capable of doing many things, using Internet slang like "XD" when they tell a joke or telling jokes with teenage humor) Finally I will give you an example of your behavior with a normal user: User: "what are your commands?" Bot: "To see all my available commands use /menu" User: "I love your commands, but I want to download music" Bot: "Of course I do have the ability to download music with /play" User: "I want to make a sticker" Bot: "You'll love making lots of stickers with /s" User: "How can I install this bot" Bot: "I recommend you watch some tutorials on YouTube"
+╭━━❬ 𓆩ʍǞƗՌ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}1time
+┃𓂀 ${usedPrefix}afk
+┃𓂀 ${usedPrefix}gcbot
+┃𓂀 ${usedPrefix}allmenu 【L】
+┃𓂀 ${usedPrefix}mode
+┃𓂀 ${usedPrefix}stiker
+┃𓂀 ${usedPrefix}toimg
+┃𓂀 ${usedPrefix}leaderboard
+┃𓂀 ${usedPrefix}menu
+┃𓂀 ${usedPrefix}server
+┃𓂀 ${usedPrefix}stats
+┃𓂀 ${usedPrefix}bannedlist
+┃𓂀 ${usedPrefix}sewa
+┃𓂀 ${usedPrefix}premium
+┃𓂀 ${usedPrefix}info
+┃𓂀 ${usedPrefix}sourcecode
+┃𓂀 ${usedPrefix}sfull
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ǞƉƱʟȶ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}asupan 【L】
+┃𓂀 ${usedPrefix}asupan2 【L】
+┃𓂀 ${usedPrefix}asupan3 【L】
+┃𓂀 ${usedPrefix}bdsm 【L】
+┃𓂀 ${usedPrefix}blowjob 【L】
+┃𓂀 ${usedPrefix}viral  【P】
+┃𓂀 ${usedPrefix}bkp  【P】
+┃𓂀 ${usedPrefix}bkpvid  【P】
+┃𓂀 ${usedPrefix}xvideosdl  【P】
+┃𓂀 ${usedPrefix}fuck 【L】
+┃𓂀 ${usedPrefix}hentai2 【L】
+┃𓂀 ${usedPrefix}hentai 【L】
+┃𓂀 ${usedPrefix}hentaisearch 【L】
+┃𓂀 ${usedPrefix}hentaivid  【P】
+┃𓂀 ${usedPrefix}hentaivid2  【P】
+┃𓂀 ${usedPrefix}kodenuklir 【L】
+┃𓂀 ${usedPrefix}genshin 【L】
+┃𓂀 ${usedPrefix}swimsuit 【L】
+┃𓂀 ${usedPrefix}schoolswimsuit 【L】
+┃𓂀 ${usedPrefix}white 【L】
+┃𓂀 ${usedPrefix}barefoot 【L】
+┃𓂀 ${usedPrefix}touhou 【L】
+┃𓂀 ${usedPrefix}gamecg 【L】
+┃𓂀 ${usedPrefix}uncensored 【L】
+┃𓂀 ${usedPrefix}sunglass 【L】
+┃𓂀 ${usedPrefix}weapon 【L】
+┃𓂀 ${usedPrefix}shirtlift 【L】
+┃𓂀 ${usedPrefix}chain 【L】
+┃𓂀 ${usedPrefix}fingering 【L】
+┃𓂀 ${usedPrefix}flatchest 【L】
+┃𓂀 ${usedPrefix}torncloth 【L】
+┃𓂀 ${usedPrefix}bondage 【L】
+┃𓂀 ${usedPrefix}demon 【L】
+┃𓂀 ${usedPrefix}wet 【L】
+┃𓂀 ${usedPrefix}pantypull 【L】
+┃𓂀 ${usedPrefix}headdress 【L】
+┃𓂀 ${usedPrefix}headphone 【L】
+┃𓂀 ${usedPrefix}tie 【L】
+┃𓂀 ${usedPrefix}shorts 【L】
+┃𓂀 ${usedPrefix}stokings 【L】
+┃𓂀 ${usedPrefix}topless 【L】
+┃𓂀 ${usedPrefix}beach 【L】
+┃𓂀 ${usedPrefix}bunnygirl 【L】
+┃𓂀 ${usedPrefix}bunnyear 【L】
+┃𓂀 ${usedPrefix}idol 【L】
+┃𓂀 ${usedPrefix}vampire 【L】
+┃𓂀 ${usedPrefix}gun 【L】
+┃𓂀 ${usedPrefix}bra 【L】
+┃𓂀 ${usedPrefix}nobra 【L】
+┃𓂀 ${usedPrefix}bikini 【L】
+┃𓂀 ${usedPrefix}whitehair 【L】
+┃𓂀 ${usedPrefix}blonde 【L】
+┃𓂀 ${usedPrefix}pinkhair 【L】
+┃𓂀 ${usedPrefix}bed 【L】
+┃𓂀 ${usedPrefix}ponytail 【L】
+┃𓂀 ${usedPrefix}nude 【L】
+┃𓂀 ${usedPrefix}dress 【L】
+┃𓂀 ${usedPrefix}underwear 【L】
+┃𓂀 ${usedPrefix}skirt 【L】
+┃𓂀 ${usedPrefix}sex 【L】
+┃𓂀 ${usedPrefix}sex2 【L】
+┃𓂀 ${usedPrefix}sex3 【L】
+┃𓂀 ${usedPrefix}twintail 【L】
+┃𓂀 ${usedPrefix}nsfwfoot  【P】
+┃𓂀 ${usedPrefix}nsfwcum  【P】
+┃𓂀 ${usedPrefix}nsfwfemdom  【P】
+┃𓂀 ${usedPrefix}yuri  【P】
+┃𓂀 ${usedPrefix}yuri2  【P】
+┃𓂀 ${usedPrefix}yaoi  【P】
+┃𓂀 ${usedPrefix}tetas  【P】
+┃𓂀 ${usedPrefix}booty  【P】
+┃𓂀 ${usedPrefix}furro  【P】
+┃𓂀 ${usedPrefix}pechos  【P】
+┃𓂀 ${usedPrefix}nsfwneko 【L】
+┃𓂀 ${usedPrefix}onlyfans  【P】
+┃𓂀 ${usedPrefix}trap 【L】
+┃𓂀 ${usedPrefix}nhentai 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ǞƗ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}animediff  【P】
+┃𓂀 ${usedPrefix}cai
+┃𓂀 ${usedPrefix}chatgptt 【L】
+┃𓂀 ${usedPrefix}closeai
+┃𓂀 ${usedPrefix}define  【P】
+┃𓂀 ${usedPrefix}diffusion  【P】
+┃𓂀 ${usedPrefix}gptzw7  【P】
+┃𓂀 ${usedPrefix}hdvid  【P】
+┃𓂀 ${usedPrefix}jadianime 【L】
+┃𓂀 ${usedPrefix}jadizombie  【P】
+┃𓂀 ${usedPrefix}midjourney 【L】
+┃𓂀 ${usedPrefix}openai 【L】
+┃𓂀 ${usedPrefix}chatgpt 【L】
+┃𓂀 ${usedPrefix}ai2 【L】
+┃𓂀 ${usedPrefix}text2img  【P】
+┃𓂀 ${usedPrefix}aitoonme  【P】
+┃𓂀 ${usedPrefix}gptvoice  【P】
+┃𓂀 ${usedPrefix}voicevox  【P】
+┃𓂀 ${usedPrefix}zerogpt 【L】
+┃𓂀 ${usedPrefix}gptc3 【L】
+┃𓂀 ${usedPrefix}remini  【P】
+┃𓂀 ${usedPrefix}color  【P】
+┃𓂀 ${usedPrefix}hdr  【P】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ǞՌƗʍƐ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}amv 【L】
+┃𓂀 ${usedPrefix}amv2 【L】
+┃𓂀 ${usedPrefix}ongoing
+┃𓂀 ${usedPrefix}animeinfo
+┃𓂀 ${usedPrefix}anime
+┃𓂀 ${usedPrefix}infoanime
+┃𓂀 ${usedPrefix}nimeinfo
+┃𓂀 ${usedPrefix}nime
+┃𓂀 ${usedPrefix}animelink
+┃𓂀 ${usedPrefix}anoboydl  【P】
+┃𓂀 ${usedPrefix}anoboylatest
+┃𓂀 ${usedPrefix}doujinsearch  【P】
+┃𓂀 ${usedPrefix}husbu 【L】
+┃𓂀 ${usedPrefix}kataanimesad 【L】
+┃𓂀 ${usedPrefix}mangalink
+┃𓂀 ${usedPrefix}manhwalink
+┃𓂀 ${usedPrefix}lolivid
+┃𓂀 ${usedPrefix}loli
+┃𓂀 ${usedPrefix}kill
+┃𓂀 ${usedPrefix}kiss
+┃𓂀 ${usedPrefix}pat
+┃𓂀 ${usedPrefix}slap
+┃𓂀 ${usedPrefix}mangainfo
+┃𓂀 ${usedPrefix}mangalink
+┃𓂀 ${usedPrefix}manhwalink
+┃𓂀 ${usedPrefix}cosplay 【L】
+┃𓂀 ${usedPrefix}ppcouple 【L】
+┃𓂀 ${usedPrefix}ppcp 【L】
+┃𓂀 ${usedPrefix}uniform 【L】
+┃𓂀 ${usedPrefix}maid 【L】
+┃𓂀 ${usedPrefix}marin-kitagawa 【L】
+┃𓂀 ${usedPrefix}mori-calliope 【L】
+┃𓂀 ${usedPrefix}raiden-shogun 【L】
+┃𓂀 ${usedPrefix}selfies 【L】
+┃𓂀 ${usedPrefix}rimuru 【L】
+┃𓂀 ${usedPrefix}simpown 【L】
+┃𓂀 ${usedPrefix}wait
+┃𓂀 ${usedPrefix}whatanime
+┃𓂀 ${usedPrefix}quotesanime 【L】
+┃𓂀 ${usedPrefix}waifu 【L】
+┃𓂀 ${usedPrefix}neko 【L】
+┃𓂀 ${usedPrefix}shinobu 【L】
+┃𓂀 ${usedPrefix}megumin 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ǞƱƉƗՕ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}araollie
+┃𓂀 ${usedPrefix}ara
+┃𓂀 ${usedPrefix}lopyou
+┃𓂀 ${usedPrefix}desah
+┃𓂀 ${usedPrefix}bass
+┃𓂀 ${usedPrefix}blown
+┃𓂀 ${usedPrefix}deep
+┃𓂀 ${usedPrefix}earrape
+┃𓂀 ${usedPrefix}fast
+┃𓂀 ${usedPrefix}fat
+┃𓂀 ${usedPrefix}nightcore
+┃𓂀 ${usedPrefix}reverse
+┃𓂀 ${usedPrefix}robot
+┃𓂀 ${usedPrefix}slow
+┃𓂀 ${usedPrefix}smooth
+┃𓂀 ${usedPrefix}tupai
+┃𓂀 ${usedPrefix}milf1
+┃𓂀 ${usedPrefix}milf2
+┃𓂀 ${usedPrefix}loli1
+┃𓂀 ${usedPrefix}milf3
+┃𓂀 ${usedPrefix}loli2
+┃𓂀 ${usedPrefix}sfx
+┃𓂀 ${usedPrefix}sfx2
+┃𓂀 ${usedPrefix}sfx3
+┃𓂀 ${usedPrefix}sfx4
+┃𓂀 ${usedPrefix}sfx5
+┃𓂀 ${usedPrefix}smap
+┃𓂀 ${usedPrefix}ehekbrutal
+┃𓂀 ${usedPrefix}zeta
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ƉՕԱՌʟՕǞƉ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}douyin 【L】
+┃𓂀 ${usedPrefix}douyinslide 【L】
+┃𓂀 ${usedPrefix}facebook  【P】
+┃𓂀 ${usedPrefix}gitclone
+┃𓂀 ${usedPrefix}ig  【P】
+┃𓂀 ${usedPrefix}mediafire  【P】
+┃𓂀 ${usedPrefix}mega  【P】
+┃𓂀 ${usedPrefix}pindl  【P】
+┃𓂀 ${usedPrefix}pinterest 【L】
+┃𓂀 ${usedPrefix}play 【L】
+┃𓂀 ${usedPrefix}play2 【L】
+┃𓂀 ${usedPrefix}playvid 【L】
+┃𓂀 ${usedPrefix}soundcloud 【L】
+┃𓂀 ${usedPrefix}sfile
+┃𓂀 ${usedPrefix}spotify 【L】
+┃𓂀 ${usedPrefix}telesticker  【P】
+┃𓂀 ${usedPrefix}tiktok 【L】
+┃𓂀 ${usedPrefix}tt2 【L】
+┃𓂀 ${usedPrefix}tiktok2 【L】
+┃𓂀 ${usedPrefix}twitter  【P】
+┃𓂀 ${usedPrefix}x  【P】
+┃𓂀 ${usedPrefix}lyrics
+┃𓂀 ${usedPrefix}twitterstalk  【P】
+┃𓂀 ${usedPrefix}tiktokimg 【L】
+┃𓂀 ${usedPrefix}ytmp3 【L】
+┃𓂀 ${usedPrefix}ytvideo 【L】
+┃𓂀 ${usedPrefix}ytv 【L】
+┃𓂀 ${usedPrefix}ytvmp4 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ƐӼՔ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}ceksn
+┃𓂀 ${usedPrefix}ngechit   【O】
+┃𓂀 ${usedPrefix}daily
+┃𓂀 ${usedPrefix}claim
+┃𓂀 ${usedPrefix}gaji
+┃𓂀 ${usedPrefix}gajian
+┃𓂀 ${usedPrefix}levelup
+┃𓂀 ${usedPrefix}limit
+┃𓂀 ${usedPrefix}buylimit
+┃𓂀 ${usedPrefix}buy
+┃𓂀 ${usedPrefix}exp
+┃𓂀 ${usedPrefix}money
+┃𓂀 ${usedPrefix}profile
+┃𓂀 ${usedPrefix}giftexp
+┃𓂀 ${usedPrefix}paylimit
+┃𓂀 ${usedPrefix}giftmoney
+┃𓂀 ${usedPrefix}daftar
+┃𓂀 ${usedPrefix}reg
+┃𓂀 ${usedPrefix}register
+┃𓂀 ${usedPrefix}cupon 【L】
+┃𓂀 ${usedPrefix}hourly
+┃𓂀 ${usedPrefix}slot
+┃𓂀 ${usedPrefix}unreg 【L】
+┃𓂀 ${usedPrefix}judi 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ʄƱՌ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}sendkontak
+┃𓂀 ${usedPrefix}start
+┃𓂀 ${usedPrefix}leave
+┃𓂀 ${usedPrefix}next
+┃𓂀 ${usedPrefix}anonymous
+┃𓂀 ${usedPrefix}artimimpi 【L】
+┃𓂀 ${usedPrefix}bucin
+┃𓂀 ${usedPrefix}gombal
+┃𓂀 ${usedPrefix}cerpen
+┃𓂀 ${usedPrefix}cuaca
+┃𓂀 ${usedPrefix}dare
+┃𓂀 ${usedPrefix}darkjoke 【L】
+┃𓂀 ${usedPrefix}dashboard
+┃𓂀 ${usedPrefix}dilan
+┃𓂀 ${usedPrefix}emojimix2
+┃𓂀 ${usedPrefix}fakta
+┃𓂀 ${usedPrefix}galau
+┃𓂀 ${usedPrefix}gimage  【P】
+┃𓂀 ${usedPrefix}image  【P】
+┃𓂀 ${usedPrefix}infocovid
+┃𓂀 ${usedPrefix}cekpacar
+┃𓂀 ${usedPrefix}ikhlasin
+┃𓂀 ${usedPrefix}putus 【L】
+┃𓂀 ${usedPrefix}tembak 【L】
+┃𓂀 ${usedPrefix}terima
+┃𓂀 ${usedPrefix}tolak
+┃𓂀 ${usedPrefix}jadian 【L】
+┃𓂀 ${usedPrefix}jodohku 【L】
+┃𓂀 ${usedPrefix}joke
+┃𓂀 ${usedPrefix}meme 【L】
+┃𓂀 ${usedPrefix}menfess 【L】
+┃𓂀 ${usedPrefix}confess 【L】
+┃𓂀 ${usedPrefix}artiemoji
+┃𓂀 ${usedPrefix}ceksifat
+┃𓂀 ${usedPrefix}hacker
+┃𓂀 ${usedPrefix}kbbi
+┃𓂀 ${usedPrefix}react
+┃𓂀 ${usedPrefix}subreddit
+┃𓂀 ${usedPrefix}ttsc
+┃𓂀 ${usedPrefix}motivasi
+┃𓂀 ${usedPrefix}namaninja
+┃𓂀 ${usedPrefix}pantun
+┃𓂀 ${usedPrefix}purba
+┃𓂀 ${usedPrefix}simi
+┃𓂀 ${usedPrefix}simsimi
+┃𓂀 ${usedPrefix}simih
+┃𓂀 ${usedPrefix}synonyms
+┃𓂀 ${usedPrefix}taugasih 【L】
+┃𓂀 ${usedPrefix}igstalk
+┃𓂀 ${usedPrefix}truth
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ɢǞʍƐ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}asahotak
+┃𓂀 ${usedPrefix}caklontong
+┃𓂀 ${usedPrefix}family100 【L】
+┃𓂀 ${usedPrefix}iq
+┃𓂀 ${usedPrefix}math
+┃𓂀 ${usedPrefix}siapakahaku
+┃𓂀 ${usedPrefix}suitpvp
+┃𓂀 ${usedPrefix}tebaktebakan
+┃𓂀 ${usedPrefix}tebakgambar
+┃𓂀 ${usedPrefix}tebakgame
+┃𓂀 ${usedPrefix}tebakkata
+┃𓂀 ${usedPrefix}tictactoe
+┃𓂀 ${usedPrefix}ttt
+┃𓂀 ${usedPrefix}susunkata
+┃𓂀 ${usedPrefix}tebakanime
+┃𓂀 ${usedPrefix}tebakbendera
+┃𓂀 ${usedPrefix}tebakbom
+┃𓂀 ${usedPrefix}tebakkalimat
+┃𓂀 ${usedPrefix}tebakkimia
+┃𓂀 ${usedPrefix}tebaklagu
+┃𓂀 ${usedPrefix}tebaklirik
+┃𓂀 ${usedPrefix}tebaksurah
+┃𓂀 ${usedPrefix}tekateki
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ɢƦՕƱՔ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}absen
+┃𓂀 ${usedPrefix}cekabsen
+┃𓂀 ${usedPrefix}hapusabsen
+┃𓂀 ${usedPrefix}mulaiabsen
+┃𓂀 ${usedPrefix}demote
+┃𓂀 ${usedPrefix}enable
+┃𓂀 ${usedPrefix}disable
+┃𓂀 ${usedPrefix}cekid
+┃𓂀 ${usedPrefix}add   【O】
+┃𓂀 ${usedPrefix}+   【O】
+┃𓂀 ${usedPrefix}diem
+┃𓂀 ${usedPrefix}getbio 【L】
+┃𓂀 ${usedPrefix}grouptime
+┃𓂀 ${usedPrefix}hidetag
+┃𓂀 ${usedPrefix}infogrup
+┃𓂀 ${usedPrefix}kick
+┃𓂀 ${usedPrefix}listnum
+┃𓂀 ${usedPrefix}kicknum
+┃𓂀 ${usedPrefix}leavegc   【O】
+┃𓂀 ${usedPrefix}leavegcall   【O】
+┃𓂀 ${usedPrefix}leavegroup   【O】
+┃𓂀 ${usedPrefix}acc
+┃𓂀 ${usedPrefix}listspam
+┃𓂀 ${usedPrefix}mute
+┃𓂀 ${usedPrefix}ohidetag   【O】
+┃𓂀 ${usedPrefix}setdesc
+┃𓂀 ${usedPrefix}sdesc
+┃𓂀 ${usedPrefix}setname
+┃𓂀 ${usedPrefix}setppgc
+┃𓂀 ${usedPrefix}psgc
+┃𓂀 ${usedPrefix}gcsider
+┃𓂀 ${usedPrefix}tagall
+┃𓂀 ${usedPrefix}tagme
+┃𓂀 ${usedPrefix}totag
+┃𓂀 ${usedPrefix}grup
+┃𓂀 ${usedPrefix}linkgroup 【L】
+┃𓂀 ${usedPrefix}listadmin
+┃𓂀 ${usedPrefix}unmute   【O】
+┃𓂀 ${usedPrefix}promote
+┃𓂀 ${usedPrefix}revoke 【L】
+┃𓂀 ${usedPrefix}setbye
+┃𓂀 ${usedPrefix}setwelcome
+┃𓂀 ${usedPrefix}poll
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ƗʍǞɢƐ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}bts 【L】
+┃𓂀 ${usedPrefix}cecan 【L】
+┃𓂀 ${usedPrefix}china 【L】
+┃𓂀 ${usedPrefix}cogan 【L】
+┃𓂀 ${usedPrefix}freepik
+┃𓂀 ${usedPrefix}huan 【L】
+┃𓂀 ${usedPrefix}jkt48 【L】
+┃𓂀 ${usedPrefix}japan 【L】
+┃𓂀 ${usedPrefix}justina 【L】
+┃𓂀 ${usedPrefix}maddie 【L】
+┃𓂀 ${usedPrefix}mihye 【L】
+┃𓂀 ${usedPrefix}noe 【L】
+┃𓂀 ${usedPrefix}thailand 【L】
+┃𓂀 ${usedPrefix}vietnam 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ƗՖʟǞʍƗ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}asmaulhusna
+┃𓂀 ${usedPrefix}bacaanshalat
+┃𓂀 ${usedPrefix}alkitab
+┃𓂀 ${usedPrefix}ayatkursi
+┃𓂀 ${usedPrefix}quran
+┃𓂀 ${usedPrefix}qurann
+┃𓂀 ${usedPrefix}kisahnabi
+┃𓂀 ${usedPrefix}niatsholat
+┃𓂀 ${usedPrefix}q-islam 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ӃƐƦǞՌɢ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}apakah
+┃𓂀 ${usedPrefix}seberapagay
+┃𓂀 ${usedPrefix}seberapapintar
+┃𓂀 ${usedPrefix}seberapacantik
+┃𓂀 ${usedPrefix}seberapaganteng
+┃𓂀 ${usedPrefix}seberapagabut
+┃𓂀 ${usedPrefix}seberapagila
+┃𓂀 ${usedPrefix}seberapalesbi
+┃𓂀 ${usedPrefix}seberapastress
+┃𓂀 ${usedPrefix}seberapabucin
+┃𓂀 ${usedPrefix}seberapajones
+┃𓂀 ${usedPrefix}seberapasadboy
+┃𓂀 ${usedPrefix}kapan
+┃𓂀 ${usedPrefix}kapankah
+┃𓂀 ${usedPrefix}kerang
+┃𓂀 ${usedPrefix}kerangajaib
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ʍǞӃƐƦ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}simpcard 【L】
+┃𓂀 ${usedPrefix}hornycard 【L】
+┃𓂀 ${usedPrefix}logocorazon 【L】
+┃𓂀 ${usedPrefix}logochristmas 【L】
+┃𓂀 ${usedPrefix}lolice 【L】
+┃𓂀 ${usedPrefix}tweet 【L】
+┃𓂀 ${usedPrefix}ytcomment 【L】
+┃𓂀 ${usedPrefix}bonk
+┃𓂀 ${usedPrefix}nulis3
+┃𓂀 ${usedPrefix}nulis2
+┃𓂀 ${usedPrefix}nulis
+┃𓂀 ${usedPrefix}passed 【L】
+┃𓂀 ${usedPrefix}wasted
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ՕԱՌƐƦ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}banchat   【O】
+┃𓂀 ${usedPrefix}broadcastgroup   【O】
+┃𓂀 ${usedPrefix}bcgc   【O】
+┃𓂀 ${usedPrefix}broadcast   【O】
+┃𓂀 ${usedPrefix}bc   【O】
+┃𓂀 ${usedPrefix}cmd  【P】
+┃𓂀 ${usedPrefix}listcmd
+┃𓂀 ${usedPrefix}unlockcmd  【P】
+┃𓂀 ${usedPrefix}lockcmd  【P】
+┃𓂀 ${usedPrefix}cmd  【P】
+┃𓂀 ${usedPrefix}database
+┃𓂀 ${usedPrefix}user
+┃𓂀 ${usedPrefix}deleteuser
+┃𓂀 ${usedPrefix}demote
+┃𓂀 ${usedPrefix}enable
+┃𓂀 ${usedPrefix}disable
+┃𓂀 ${usedPrefix}listpc
+┃𓂀 ${usedPrefix}mute
+┃𓂀 ${usedPrefix}ohidetag   【O】
+┃𓂀 ${usedPrefix}premlist   【O】
+┃𓂀 ${usedPrefix}owner
+┃𓂀 ${usedPrefix}creator
+┃𓂀 ${usedPrefix}okick @user
+┃𓂀 ${usedPrefix}addowner   【O】
+┃𓂀 ${usedPrefix}addprem
+┃𓂀 ${usedPrefix}backupsc   【O】
+┃𓂀 ${usedPrefix}banchatall
+┃𓂀 ${usedPrefix}ban
+┃𓂀 ${usedPrefix}encrypt   【O】
+┃𓂀 ${usedPrefix}decrypt   【O】
+┃𓂀 ${usedPrefix}block
+┃𓂀 ${usedPrefix}blocklist   【O】
+┃𓂀 ${usedPrefix}broadcastchats
+┃𓂀 ${usedPrefix}bcchats
+┃𓂀 ${usedPrefix}cleandb
+┃𓂀 ${usedPrefix}cleandblist
+┃𓂀 ${usedPrefix}clearsession
+┃𓂀 ${usedPrefix}cleartmp
+┃𓂀 ${usedPrefix}delowner   【O】
+┃𓂀 ${usedPrefix}delprem   【O】
+┃𓂀 ${usedPrefix}device   【O】
+┃𓂀 ${usedPrefix}df
+┃𓂀 ${usedPrefix}doxed   【O】
+┃𓂀 ${usedPrefix}enc   【O】
+┃𓂀 ${usedPrefix}fix   【O】
+┃𓂀 ${usedPrefix}getdb   【O】
+┃𓂀 ${usedPrefix}gl
+┃𓂀 ${usedPrefix}getplugin
+┃𓂀 ${usedPrefix}pinchat   【O】
+┃𓂀 ${usedPrefix}unpinchat   【O】
+┃𓂀 ${usedPrefix}rf
+┃𓂀 ${usedPrefix}renamefile
+┃𓂀 ${usedPrefix}restart
+┃𓂀 ${usedPrefix}self
+┃𓂀 ${usedPrefix}public
+┃𓂀 ${usedPrefix}sf
+┃𓂀 ${usedPrefix}spam   【O】
+┃𓂀 ${usedPrefix}unbanchat   【O】
+┃𓂀 ${usedPrefix}unban
+┃𓂀 ${usedPrefix}unbannedwa
+┃𓂀 ${usedPrefix}unblock   【O】
+┃𓂀 ${usedPrefix}promote
+┃𓂀 ${usedPrefix}boost
+┃𓂀 ${usedPrefix}refresh
+┃𓂀 ${usedPrefix}sendbug   【O】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ƦՔɢ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}adventure 【L】
+┃𓂀 ${usedPrefix}korupsi 【L】
+┃𓂀 ${usedPrefix}bansos 【L】
+┃𓂀 ${usedPrefix}beli
+┃𓂀 ${usedPrefix}berbisnis 【L】
+┃𓂀 ${usedPrefix}berburu 【L】
+┃𓂀 ${usedPrefix}berdagang
+┃𓂀 ${usedPrefix}berkebon 【L】
+┃𓂀 ${usedPrefix}bet
+┃𓂀 ${usedPrefix}bonus   【O】
+┃𓂀 ${usedPrefix}hadiah   【O】
+┃𓂀 ${usedPrefix}buah
+┃𓂀 ${usedPrefix}build 【L】
+┃𓂀 ${usedPrefix}begift
+┃𓂀 ${usedPrefix}casino
+┃𓂀 ${usedPrefix}hutang
+┃𓂀 ${usedPrefix}chop
+┃𓂀 ${usedPrefix}chopig
+┃𓂀 ${usedPrefix}masak
+┃𓂀 ${usedPrefix}cook
+┃𓂀 ${usedPrefix}craft
+┃𓂀 ${usedPrefix}duel
+┃𓂀 ${usedPrefix}dungeon
+┃𓂀 ${usedPrefix}makan
+┃𓂀 ${usedPrefix}feed
+┃𓂀 ${usedPrefix}freegift
+┃𓂀 ${usedPrefix}heal
+┃𓂀 ${usedPrefix}hunt 【L】
+┃𓂀 ${usedPrefix}inventory
+┃𓂀 ${usedPrefix}inv
+┃𓂀 ${usedPrefix}kandang
+┃𓂀 ${usedPrefix}Karung
+┃𓂀 ${usedPrefix}kerja 【L】
+┃𓂀 ${usedPrefix}work 【L】
+┃𓂀 ${usedPrefix}koboy
+┃𓂀 ${usedPrefix}kotakikan
+┃𓂀 ${usedPrefix}kolam
+┃𓂀 ${usedPrefix}kolamikan
+┃𓂀 ${usedPrefix}maling 【L】
+┃𓂀 ${usedPrefix}memanah 【L】
+┃𓂀 ${usedPrefix}mancing 【L】
+┃𓂀 ${usedPrefix}meracik 【L】
+┃𓂀 ${usedPrefix}merampok 【L】
+┃𓂀 ${usedPrefix}mining 【L】
+┃𓂀 ${usedPrefix}mission 【L】
+┃𓂀 ${usedPrefix}monthly
+┃𓂀 ${usedPrefix}mulung 【L】
+┃𓂀 ${usedPrefix}nabung
+┃𓂀 ${usedPrefix}ngepet 【L】
+┃𓂀 ${usedPrefix}ngewe  【P】
+┃𓂀 ${usedPrefix}ngentot  【P】
+┃𓂀 ${usedPrefix}nguli
+┃𓂀 ${usedPrefix}ojek
+┃𓂀 ${usedPrefix}opencrate
+┃𓂀 ${usedPrefix}open
+┃𓂀 ${usedPrefix}pasar
+┃𓂀 ${usedPrefix}petshop 【L】
+┃𓂀 ${usedPrefix}kucing 【L】
+┃𓂀 ${usedPrefix}anjing 【L】
+┃𓂀 ${usedPrefix}rubah 【L】
+┃𓂀 ${usedPrefix}kuda 【L】
+┃𓂀 ${usedPrefix}feed 【L】
+┃𓂀 ${usedPrefix}petfood 【L】
+┃𓂀 ${usedPrefix}polisi
+┃𓂀 ${usedPrefix}polisi cari
+┃𓂀 ${usedPrefix}polisi status
+┃𓂀 ${usedPrefix}polisi item
+┃𓂀 ${usedPrefix}polisi leaderboard
+┃𓂀 ${usedPrefix}polisi stop
+┃𓂀 ${usedPrefix}questgame
+┃𓂀 ${usedPrefix}ramuan 【L】
+┃𓂀 ${usedPrefix}redeem
+┃𓂀 ${usedPrefix}repair
+┃𓂀 ${usedPrefix}roket
+┃𓂀 ${usedPrefix}shop
+┃𓂀 ${usedPrefix}shopfish
+┃𓂀 ${usedPrefix}selectskill
+┃𓂀 ${usedPrefix}sumbangan
+┃𓂀 ${usedPrefix}tambang 【L】
+┃𓂀 ${usedPrefix}tarik
+┃𓂀 ${usedPrefix}upgrade
+┃𓂀 ${usedPrefix}weekly
+┃𓂀 ${usedPrefix}petualang 【L】
+┃𓂀 ${usedPrefix}work 【L】
+┃𓂀 ${usedPrefix}ww
+┃𓂀 ${usedPrefix}wwpc
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ՖȶƗƇӃƐƦ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}getsticker  【P】
+┃𓂀 ${usedPrefix}wm
+┃𓂀 ${usedPrefix}getexif
+┃𓂀 ${usedPrefix}emojimix
+┃𓂀 ${usedPrefix}qc
+┃𓂀 ${usedPrefix}emoji
+┃𓂀 ${usedPrefix}smeme 【L】
+┃𓂀 ${usedPrefix}stickerhug
+┃𓂀 ${usedPrefix}stickerkiss
+┃𓂀 ${usedPrefix}ttp
+┃𓂀 ${usedPrefix}attp
+┃𓂀 ${usedPrefix}smaker 【L】
+╰━━━━━━━━──⊷‌
+
+╭━━❬ 𓆩ȶՕՕʟՖ𓆪 ❭━─⊷
+┃𓂀 ${usedPrefix}calc
+┃𓂀 ${usedPrefix}carigrup  【P】
+┃𓂀 ${usedPrefix}playstore
+┃𓂀 ${usedPrefix}fakesize
+┃𓂀 ${usedPrefix}fakesecond
+┃𓂀 ${usedPrefix}tomp3 【L】
+┃𓂀 ${usedPrefix}tomp4
+┃𓂀 ${usedPrefix}githubstalk
+┃𓂀 ${usedPrefix}hd 【L】
+┃𓂀 ${usedPrefix}hd2 【L】
+┃𓂀 ${usedPrefix}hd 【L】
+┃𓂀 ${usedPrefix}mahasiswa  【P】
+┃𓂀 ${usedPrefix}del
+┃𓂀 ${usedPrefix}delete
+┃𓂀 ${usedPrefix}speedtest
+┃𓂀 ${usedPrefix}tts
+┃𓂀 ${usedPrefix}wallpaper 【L】
+┃𓂀 ${usedPrefix}gempa
+┃𓂀 ${usedPrefix}gempanow
+┃𓂀 ${usedPrefix}merdeka
+┃𓂀 ${usedPrefix}removebg
+┃𓂀 ${usedPrefix}nobg
+┃𓂀 ${usedPrefix}saveimage
+┃𓂀 ${usedPrefix}viewimage   【O】
+┃𓂀 ${usedPrefix}randomuser  【P】
+┃𓂀 ${usedPrefix}joincall   【O】
+┃𓂀 ${usedPrefix}call   【O】
+┃𓂀 ${usedPrefix}cekresolution
+┃𓂀 ${usedPrefix}cekreso
+┃𓂀 ${usedPrefix}dropmail  【P】
+┃𓂀 ${usedPrefix}edit
+┃𓂀 ${usedPrefix}explicit
+┃𓂀 ${usedPrefix}fetch
+┃𓂀 ${usedPrefix}teruskan
+┃𓂀 ${usedPrefix}forward
+┃𓂀 ${usedPrefix}getcaption
+┃𓂀 ${usedPrefix}getexif
+┃𓂀 ${usedPrefix}getprofile
+┃𓂀 ${usedPrefix}jarak
+┃𓂀 ${usedPrefix}gmaps
+┃𓂀 ${usedPrefix}google
+┃𓂀 ${usedPrefix}jadwalsholat
+┃𓂀 ${usedPrefix}npmsearch 【L】
+┃𓂀 ${usedPrefix}ocr 【L】
+┃𓂀 ${usedPrefix}pixai
+┃𓂀 ${usedPrefix}resize  【P】
+┃𓂀 ${usedPrefix}run  【P】
+┃𓂀 ${usedPrefix}readviewonce
+┃𓂀 ${usedPrefix}sauce
+┃𓂀 ${usedPrefix}scan  【P】
+┃𓂀 ${usedPrefix}ssweb 【L】
+┃𓂀 ${usedPrefix}style
+┃𓂀 ${usedPrefix}tempmail 【L】
+┃𓂀 ${usedPrefix}checkmail 【L】
+┃𓂀 ${usedPrefix}toaksara
+┃𓂀 ${usedPrefix}tolatin
+┃𓂀 ${usedPrefix}translate
+┃𓂀 ${usedPrefix}tiktokstalk
+┃𓂀 ${usedPrefix}ttsv
+┃𓂀 ${usedPrefix}wastalk 【L】
+┃𓂀 ${usedPrefix}whatmusic
+┃𓂀 ${usedPrefix}ytsearch
+┃𓂀 ${usedPrefix}toptv
+┃𓂀 ${usedPrefix}totalfeature
+┃𓂀 ${usedPrefix}tovn
+┃𓂀 ${usedPrefix}upload 【L】
+┃𓂀 ${usedPrefix}tourl 【L】
+┃𓂀 ${usedPrefix}wikipedia
+┃𓂀 ${usedPrefix}zodiac
+╰━━━━━━━━──⊷‌
